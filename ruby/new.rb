c6fe6fac2f90bc8c2ef1fcd7f41484bb92c06c22
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-05-14 03:15:00 +0000

    [start]5-23

---
+++ ruby/new.rb
@@ -0,0 +1,27 @@
+class Car
+  
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
+  def turn(direction)
+    puts"#{direction}に曲がります。"
+  end
+  
+  def run(distance)
+    puts"車で#{distance}キロ走ります。"
+  end
+end
+
+class Car
+ def self.run(distance)
+  puts"車で#{distance}キロ走ります。"
+ end
+ def self.turn(direction)
+  puts"車で#{direction}に曲がります。"
+ end
+end
+
+Car.run(10)
+Car.turn("右")
