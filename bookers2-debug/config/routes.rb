bd70cd6094decbcb49d4aa209ef411671e870156
sunny0105yoko <ec2-user@ip-172-31-26-60.ap-northeast-1.compute.internal>
2023-05-30 05:33:34 +0000

    [start]応用課題４

--- bookers2-debug/config/routes.rb
+++ bookers2-debug/config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers' ,as: 'follower'
+    get 'followers' => 'relationships#followers' ,as: 'followers'
   end
 
 
